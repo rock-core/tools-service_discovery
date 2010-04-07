986a8cf66e034e91d53b77a892bd1d79a4a8a6c2
darko.makreshanski@dfki.de <darko@darko-laptop.(none)>
2010-04-07 15:39:07 +0200

    documentation

---
+++ examples/browse.cpp
@@ -0,0 +1,51 @@
+/**
+ * a example source file for browsing services with the avahi c++ framework
+ */
+ 
+#include <stdlib.h>
+#include <iostream>
+#include <service-discovery/afAvahiFramework.h>
+ 
+void testAdded (afRemoteService* rms) {
+	std::cout << " -=- TESTING SIGNAL: ADDED SERVICE: " << rms->getName() << std::endl;
+}
+
+void testRemoved (afRemoteService rms) {
+	std::cout << " -=- TESTING SIGNAL: REMOVED SERVICE: " << rms.getName() << std::endl;
+}
+
+ 
+int main(int argc, char** argv)
+{
+	
+	//create a client with default constructor with threaded poll
+	afAvahiClient client;
+	
+	//create a service browser
+	afServiceBrowser sbrowser(&client, "_rimres._tcp");
+
+	//connect a callback to the service added signal. Method can also be a class member. Look at sigc++ api
+	sbrowser.afServiceAdded.connect(sigc::ptr_fun(testAdded));
+	
+	//connect a callback to the service removed signal
+	sbrowser.afServiceRemoved.connect(sigc::ptr_fun(testRemoved));
+
+	//publish a sample service to test the callbacks
+	std::list<std::string> strlst;
+	strlst.push_back("service_year=1999");
+	afLocalService serv(&client, "MyTestService", "_rimres._tcp", 10000, strlst);
+	
+	//run the main event loop (in this case in a different thread because default poll is afThreadPoll, so it program will continue normal execution)
+	client.dispatch();
+	
+	sleep(3);
+	
+	//unpublish the service and check for the signal
+	serv.unpublish();
+	
+	sleep(2);
+	
+	//stop main loop. not really needed if nothing is done afterwards
+	client.stop();	
+	
+}
---
+++ examples/publish.cpp
@@ -0,0 +1,26 @@
+/**
+ * a example source file for publishing services with the avahi c++ framework
+ */
+ 
+#include <stdlib.h>
+#include <service-discovery/afAvahiFramework.h>
+ 
+int main(int argc, char** argv)
+{
+	
+	//create a client with default constructor with threaded poll
+	afAvahiClient client;
+	
+	std::list<std::string> strlst;
+	strlst.push_back("service_year=1999");
+	afLocalService serv(&client, "MyTestService", "_mytype._tcp", 10000, strlst);
+	
+	//run the main event loop (in this case in a different thread because default poll is afThreadPoll, so it program will continue normal execution)
+	client.dispatch();
+	
+	sleep(10);
+	
+	//stop main loop. not really needed if nothing is done afterwards
+	client.stop();	
+	
+}
