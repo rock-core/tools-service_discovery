e3c9f349fab56ce2f820e883adc94acf8f2e2c1b
Thomas Roehr <thomas.roehr@dfki.de>
2010-11-23 19:46:21 +0100

    Fix missing -I in pkgconfig file

--- cmake/CommonSetup.cmake
+++ cmake/CommonSetup.cmake
@@ -1,7 +1,9 @@
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/templates")
+
 if(DEFINED CMAKE_BUILD_TYPE)
-   set(CMAKE_BUILD_TYPE ${CMAKE_BUILD_TYPE} CACHE STRING "Choose the type of
-build, options are: None(CMAKE_CXX_FLAGS or CMAKE_C_FLAGS used) Debug
-Release RelWithDebInfo MinSizeRel.")
+#   set(CMAKE_BUILD_TYPE ${CMAKE_BUILD_TYPE} CACHE STRING "Choose the type of
+# build, options are: None(CMAKE_CXX_FLAGS or CMAKE_C_FLAGS used) Debug
+# Release RelWithDebInfo MinSizeRel.")
 else()
    ##### Build types #################################################
    # single-configuration generator like Makefile generator creates following variables per default
@@ -12,9 +14,9 @@ else()
    # RelWithDebInfo (CMAKE_C_FLAGS_RELWITHDEBINFO or CMAKE_CXX_FLAGS_RELWITHDEBINFO
    # MinSizeRel (CMAKE_C_FLAGS_MINSIZEREL or CMAKE_CXX_FLAGS_MINSIZEREL) 
    ####################################################################
-   set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build,
-options are: None(CMAKE_CXX_FLAGS or CMAKE_C_FLAGS used) Debug Release
-RelWithDebInfo MinSizeRel.")
+   set(CMAKE_BUILD_TYPE Debug)
+# Cache only works for the cmake gui
+# CACHE STRING "Choose the type of build, options are: None(CMAKE_CXX_FLAGS or CMAKE_C_FLAGS used) Debug Release RelWithDebInfo MinSizeRel.")
 endif()
 
 message(STATUS "Build type set to: " ${CMAKE_BUILD_TYPE})
@@ -33,7 +35,11 @@ if(DOXYGEN_EXECUTABLE)
     ADD_CUSTOM_TARGET(doc ${DOXYGEN_EXECUTABLE} ${PROJECT_SOURCE_DIR}/doc/Doxyfile)
     # generates documentation with cmake
     # QUIET mode is enabled in the configuration file (QUIET = YES)
-    EXECUTE_PROCESS(COMMAND ${DOXYGEN_EXECUTABLE} ${PROJECT_SOURCE_DIR}/doc/Doxyfile )
+    # If you want to autogenerate you documentation use set(AUTOGENERATE_DOC True) in the main 
+    # CMakeList.txt file
+    if(DEFINED AUTOGENERATE_DOC)
+	    EXECUTE_PROCESS(COMMAND ${DOXYGEN_EXECUTABLE} ${PROJECT_SOURCE_DIR}/doc/Doxyfile )
+    endif()
 endif(DOXYGEN_EXECUTABLE)
 ##### End doxygen support ###################################################
 
@@ -56,15 +62,20 @@ install(DIRECTORY ${PROJECT_SOURCE_DIR}/src/ DESTINATION include/${PROJECT_NAME}
 # COPY Configuration files into build directory. Workaround: remove the .pc.in
 # file, as we don't want to install it
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/configuration ${PROJECT_BINARY_DIR}/configuration)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove -f ${PROJECT_BINARY_DIR}/configuration/${PROJECT_NAME}.pc.in)
+
+# install configuration files
+install(DIRECTORY ${PROJECT_BINARY_DIR}/configuration/ DESTINATION configuration/${PROJECT_NAME}
+	FILES_MATCHING PATTERN "*.properties")
+
+# Workaround: Cleanup the in file from build directory
+# First making sure we are not in the source directory, otherwise we can delete the in files
+string(COMPARE NOTEQUAL "${PROJECT_SOURCE_DIR}" "${PROJECT_BINARY_DIR}" BUILDING_IN_SRC_DIR)
+if ( ${BUILDING_IN_SRC_DIR} )
+		execute_process(COMMAND ${CMAKE_COMMAND} -E remove -f ${PROJECT_BINARY_DIR}/configuration/${PROJECT_NAME}.pc.in)
+else ( ${BUILDING_IN_SRC_DIR} )
+    message("WARNING: Your are building in the source directory. Recommending to abort and using a designated build directory.") 
+endif( ${BUILDING_IN_SRC_DIR} )
 
 # scripts/
 # Install the scripts
-install(DIRECTORY ${PROJECT_SOURCE_DIR}/scripts/ DESTINATION scripts)
-
-
-# Generate and install the pkg-config file
-configure_file(${PROJECT_SOURCE_DIR}/configuration/${PROJECT_NAME}.pc.in
-		${PROJECT_BINARY_DIR}/configuration/${PROJECT_NAME}.pc @ONLY)
-install(FILES ${CMAKE_BINARY_DIR}/configuration/${PROJECT_NAME}.pc DESTINATION lib/pkgconfig)
-
+install(DIRECTORY ${PROJECT_SOURCE_DIR}/scripts/ DESTINATION scripts/${PROJECT_NAME} )
