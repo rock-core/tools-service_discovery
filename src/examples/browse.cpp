ec9d950f3f96285d0188638bdde3bee201273ef5
Thomas Roehr <thomas.roehr@dfki.de>
2012-08-24 17:05:55 +0200

    Update examples using ServiceDiscovery class as main interface

--- src/examples/browse.cpp
+++ src/examples/browse.cpp
@@ -10,14 +10,6 @@
 
 using namespace servicediscovery;
 
-/*void testAdded (ServiceEvent rms) {
-	std::cout << " -=- TESTING SIGNAL: ADDED SERVICE: " << rms.getName() << std::endl;
-}
-
-void testRemoved (servicediscovery::afRemoteService rms) {
-	std::cout << " -=- TESTING SIGNAL: REMOVED SERVICE: " << rms.getName() << std::endl;
-}
-*/
 void callbackFunction (ServiceEvent rms) {
 	std::cout << " -=- TESTING SIGNAL: ADDED SERVICE: " << rms.getServiceConfiguration().getName() << std::endl;
 }
@@ -35,47 +27,4 @@ int main(int argc, char** argv)
 	sleep(150);
 
 	return 0;
-/*	//create a client with default constructor with threaded poll
-	servicediscovery::afAvahiClient client;
-	
-	//create a service browser
-	servicediscovery::afServiceBrowser sbrowser(&client, "_sd_test._tcp");
-
-	//connect a callback to the service added signal. Method can also be a class member. Look at sigc++ api
-	sbrowser.serviceAddedConnect(sigc::ptr_fun(testAdded));
-	
-	//connect a callback to the service removed signal
-	sbrowser.serviceRemovedConnect(sigc::ptr_fun(testRemoved));
-
-	//publish a sample service to test the callbacks
-	std::list<std::string> strlst;
-	strlst.push_back("service_year=1999");
-	servicediscovery::afLocalService serv(&client, "MyTestService", "_sd_test._tcp", 10000, strlst);
-	
-	//run the main event loop (in this case in a different thread because default poll is afThreadPoll, so it program will continue normal execution)
-	client.dispatch();
-	
-	sleep(3);
-	
-	//print services
-	std::cout << "PRINTING SERVICES:\n";
-	servicediscovery::afList<servicediscovery::afRemoteService> services = sbrowser.getServices();
-	servicediscovery::afList<servicediscovery::afRemoteService>::iterator it;
-	for (it = services.begin() ; it != services.end() ; it++) {
-		std::cout << "     SERVICE: " << (*it).getName() << " " << (*it).getType() << " " << (*it).getInterface() << std::endl;
-	}
-	std::cout << "FINISHED PRINTING SERVICES.\n";
-	
-	sleep(2);
-	
-	//unpublish the service and check for the signal. first must lock the object to prevent concurrency issues
-	((servicediscovery::afThreadPoll*)client.getPoll())->lock();
-	serv.unpublish();
-	((servicediscovery::afThreadPoll*)client.getPoll())->unlock();
-	
-	sleep(2);
-	
-	//stop main loop. not really needed if nothing is done afterwards
-	client.stop();	
-*/	
 }
--- src/examples/publish.cpp
+++ src/examples/publish.cpp
@@ -8,29 +8,27 @@
 #include <service_discovery/service_discovery.h>
 
 using namespace std;
+using namespace servicediscovery;
  
 int main(int argc, char** argv)
 {
 	
 	//create a client with default constructor with threaded poll
-	servicediscovery::Client* client = servicediscovery::Client::getInstance();
         cout << "Publish a service named MyTestService" << endl;
 	
-	std::list<std::string> strlst;
-	strlst.push_back("service_year=1999");
-	strlst.push_back("anotherinfo=infohere");
 	//publish a service on the network with arguments as: name, type, port, info, time to live in case of network/daemon failure
-	servicediscovery::LocalService serv(client, "MyTestService", "_mytype._tcp", 10000, strlst, 300);
+        ServiceConfiguration conf("MyTestService", "_mytype._tcp", 10000);
+	conf.setDescription("service_year","1999");
+	conf.setDescription("anotherinfo","infohere");
+        conf.setTTL(300);
+
+        ServiceDiscovery service;
+        service.start(conf);
 	
 	sleep(10);
-	
-	client->lock();
-	strlst.push_back("somethingelse=10");
+
+	conf.setDescription("somethingelse","10");
 	cout << "Updating string list\n";
-	serv.updateStringList(strlst);
-	client->unlock();
-
+	service.update(conf);
 	sleep(10);
-
-	
 }
