516f04ae8dbd95b54f1594d36871a62792256ca5
Thomas Roehr <thomas.roehr@dfki.de>
2014-02-26 16:07:01 +0100

    ruby: add defaults for Avahi::ServiceDiscovery::find_services

--- bindings/ruby/lib/servicediscovery/core.rb
+++ bindings/ruby/lib/servicediscovery/core.rb
@@ -2,10 +2,16 @@ require 'servicediscovery_ruby'
 
 module Avahi
     class ServiceDiscovery
-        # Search for service using pattern match on 
+        # Search for services using pattern match on
         # servicename, label and text within the description data, 
         # or the type (such such as _test._tcp)
-        def find_services(servicename, label = ".*", txt = ".*", type = ".*")
+        # by default searches for all available services
+        # @param [ String ] service name pattern
+        # @param [ String ] label pattern to match label of a description entry
+        # @param [ String ] txt pattern to match to content of a description
+        # @param [ String ] service type pattern
+        # @return [Array<Avahi::ServiceDescription>]
+        def find_services(servicename = ".*", label = ".*", txt = ".*", type = ".*")
             services = do_find_services(servicename, label, txt, type)
             services
         end
